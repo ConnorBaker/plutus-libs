eeee2e24eab8ca49926447859be8ca0a754b39a8
Connor Baker <connor.baker@tweag.io>
2022-11-17 17:07:47 +0000

    flake: Include cabal-cache

--- flake.nix
+++ flake.nix
@@ -3,8 +3,7 @@
   nixConfig = {
     extra-substituters = ["https://cache.iog.io" "https://iohk.cachix.org"];
     extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="];
-    allow-import-from-derivation = "true";
-    allowBroken = true;
+    allow-import-from-derivation = true;
   };
   inputs = {
     haskell-nix.url = "github:input-output-hk/haskell.nix/4ee7270856a6ba4617c7e51a6c619f365faad894";
@@ -17,6 +16,25 @@
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = haskell-nix.legacyPackages.${system};
+      haskellNixPkgs = pkgs.haskell-nix;
+
+      ghcVersion = "ghc810420210212";
+      ghc = haskellNixPkgs.compiler.${ghcVersion};
+      cabal-install = haskellNixPkgs.cabal-install.${ghcVersion};
+
+      # Build cabal-cache from hackage because it has dependencies marked as broken on nixpkgs.
+      # This also lets us use the same version of GHC we'll use to build everything else.
+      inherit
+        ((haskellNixPkgs.hackage-package {
+            name = "cabal-cache";
+            compiler-nix-name = ghcVersion;
+            version = "1.0.4.0";
+            index-state = "2022-05-02T00:00:00Z";
+          })
+          .components
+          .exes)
+        cabal-cache
+        ;
     in {
       devShells.default = pkgs.mkShell {
         buildInputs =
@@ -27,10 +45,7 @@
             pkg-config
             zlib
           ])
-          ++ (with pkgs.haskell-nix; [
-            cabal-install.ghc810420210212
-            compiler.ghc810420210212
-          ]);
+          ++ [ghc cabal-install cabal-cache];
       };
       formatter = pkgs.alejandra;
     });
